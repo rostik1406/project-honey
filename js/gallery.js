381ae82d36a0f15e0130684ec07aa3a1e37349f8
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-09-26 18:07:07 +0300

    ю

--- js/gallery.js
+++ js/gallery.js
@@ -6,15 +6,63 @@
 //   expandImg.parentElement.style.display = 'block';
 // }
 
-function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById('expandedImg');
-  // Get the image text
-  var imgText = document.getElementById('imgtext');
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = 'block';
+// ьььььььььььььььььььььььььььььььььььььььььььььььььььььь
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector('.js-gallery');
+
+const cardsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `;
+  })
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', cardsMarkup);
+galleryList.addEventListener('click', onGalleryListClick);
+
+let instance;
+
+function onGalleryListClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const url = getOriginalImgUrl(e);
+  instance = createModal(url);
+
+  instance.show(window.addEventListener('keydown', onEscClick));
+}
+
+function getOriginalImgUrl(e) {
+  return e.target.dataset.source;
+}
+
+function createModal(url) {
+  return basicLightbox.create(`<img src="${url}">`, {
+    onClose: () => {
+      window.removeEventListener('keydown', onEscClick);
+    },
+  });
+}
+
+function onEscClick(e) {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
 }
