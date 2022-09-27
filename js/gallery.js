a8bf3ee91108e71e30f0dd95f846800bbd2f2c6d
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-09-28 01:12:58 +0300

    .

--- js/gallery.js
+++ js/gallery.js
@@ -7,62 +7,62 @@
 // }
 
 // ьььььььььььььььььььььььььььььььььььььььььььььььььььььь
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-const galleryList = document.querySelector('.js-gallery');
+// const galleryList = document.querySelector('.js-gallery');
 
-const cardsMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-        <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
-        </div>
-        `;
-  })
-  .join('');
+// const cardsMarkup = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `
+//         <div class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//             <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//             />
+//         </a>
+//         </div>
+//         `;
+//   })
+//   .join('');
 
-galleryList.insertAdjacentHTML('afterbegin', cardsMarkup);
-galleryList.addEventListener('click', onGalleryListClick);
+// galleryList.insertAdjacentHTML('afterbegin', cardsMarkup);
+// galleryList.addEventListener('click', onGalleryListClick);
 
-let instance;
+// let instance;
 
-function onGalleryListClick(e) {
-  e.preventDefault();
+// function onGalleryListClick(e) {
+//   e.preventDefault();
 
-  if (!e.target.classList.contains('gallery__image')) {
-    return;
-  }
+//   if (!e.target.classList.contains('gallery__image')) {
+//     return;
+//   }
 
-  const url = getOriginalImgUrl(e);
-  instance = createModal(url);
+//   const url = getOriginalImgUrl(e);
+//   instance = createModal(url);
 
-  instance.show(window.addEventListener('keydown', onEscClick));
-}
+//   instance.show(window.addEventListener('keydown', onEscClick));
+// }
 
-function getOriginalImgUrl(e) {
-  return e.target.dataset.source;
-}
+// function getOriginalImgUrl(e) {
+//   return e.target.dataset.source;
+// }
 
-function createModal(url) {
-  return basicLightbox.create(`<img src="${url}">`, {
-    onClose: () => {
-      window.removeEventListener('keydown', onEscClick);
-    },
-  });
-}
+// function createModal(url) {
+//   return basicLightbox.create(`<img src="${url}">`, {
+//     onClose: () => {
+//       window.removeEventListener('keydown', onEscClick);
+//     },
+//   });
+// }
 
-function onEscClick(e) {
-  if (e.code === 'Escape') {
-    instance.close();
-  }
-}
+// function onEscClick(e) {
+//   if (e.code === 'Escape') {
+//     instance.close();
+//   }
+// }
